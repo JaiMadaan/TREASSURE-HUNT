ea7110a74a37590015795b4ddc056649f952d46d
JaiMadaan <77677308+JaiMadaan@users.noreply.github.com>
2021-05-18 12:58:04 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,34 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "HUNT"
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("U T N I O C N F", 700,150)
+fill("lightblue")
+text("Hint: Always changing, not constant !!", 700,170)
+
+
+fill("white")
+textSize(15)
+text("A T A A S B E D", 100,200)
+fill("lightblue")
+text("Hint: Always changing, not constant !!", 100,220)
+
+fill("white")
+textSize(15)
+text("U T N H ", 700,300)
+fill("lightblue")
+text("Hint: Always changing, not constant !!", 700,320)
+
+}
